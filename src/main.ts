d92ae6b519750cddb634ac13bdc4602850c3b2dd
Strikes <yanis.bendahmane@supinternet.fr>
2019-01-28 16:57:26 +0100

    Removed VueX package

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import Vue from "vue";
-import Vuex from "vuex";
 import App from "./App.vue";
-import { store } from "@/store";
 import router from "./router";
 import Buefy from "buefy";
 import "buefy/dist/buefy.css";
@@ -10,16 +8,12 @@ import { faSearch, faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 library.add(faSearch, faStar);
-
 Vue.component("font-awesome-icon", FontAwesomeIcon);
-
 Vue.use(Buefy, { defaultIconPack: "fas" });
-Vue.use(Vuex);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  //store,
   router,
   render: h => h(App)
 }).$mount("#app");
